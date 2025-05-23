1ac88d9563cc383cad703119dcafc7d54bd8cbf0
mantakanusaiba <123790428+mantakanusaiba@users.noreply.github.com>
2025-05-23 14:22:50 +0000

    API added for Recommended Videos

--- src/Components/PlayVideo/PlayVideo.jsx
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -6,8 +6,12 @@ import share from "../../assets/share.png";
 import save from "../../assets/save.png";
 import { API_KEY, value_converter} from '../../data';
 import moment from 'moment';
+import { useParams } from 'react-router-dom';
 
-const PlayVideo = ({videoId}) => {
+const PlayVideo = () => {
+   
+  const {videoId} = useParams();
+
   const[apiData,setApiData]= useState(null);
   const[channelData,setChannelData]= useState(null);
   const [commentData,setCommentData] = useState([]);
@@ -28,7 +32,7 @@ const PlayVideo = ({videoId}) => {
 
   useEffect(()=>{
     fetchVideoData();
-  },[])
+  },[videoId])
 
 useEffect(()=>{
     fetchOtherData();
