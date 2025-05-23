7d91be4fcfa49692d1fa98c559be09b7418a0e58
mantakanusaiba <123790428+mantakanusaiba@users.noreply.github.com>
2025-05-23 13:18:57 +0000

    API added for Video

--- src/Components/PlayVideo/PlayVideo.jsx
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -1,77 +1,84 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './PlayVideo.css'
-import video1 from "../../assets/video.mp4";
 import like from "../../assets/like.png";
 import dislike from "../../assets/dislike.png";
 import share from "../../assets/share.png";
 import save from "../../assets/save.png";
-import jack from "../../assets/jack.png";
-import user_profile from "../../assets/user_profile.jpg";
+import { API_KEY, value_converter} from '../../data';
+import moment from 'moment';
+
+const PlayVideo = ({videoId}) => {
+  const[apiData,setApiData]= useState(null);
+  const[channelData,setChannelData]= useState(null);
+  const [commentData,setCommentData] = useState([]);
+  
+  const fetchVideoData= async ()=>{
+    const videoDetails_url= `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${videoId}&key=${API_KEY}`;
+     await fetch(videoDetails_url).then(res=>res.json()).then(data=>setApiData(data.items[0]))
+  }
+
+   const fetchOtherData= async ()=>{
+     const channelData_url = `https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2Cstatistics&id=${apiData.snippet.channelId}&key=${API_KEY}`;
+     await fetch(channelData_url).then(res=>res.json()).then(data=>setChannelData(data.items[0]))
+    
+     const comment_url =`https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&maxResults=50&videoId=${videoId}&key=${API_KEY}`;
+     await fetch(comment_url).then(res=>res.json()).then(data=>setCommentData(data.items))
+  }
 
 
-const PlayVideo = () => {
+  useEffect(()=>{
+    fetchVideoData();
+  },[])
+
+useEffect(()=>{
+    fetchOtherData();
+  },[apiData])
+
+
   return (
     <div className='play-video'>
-      <video src={video1} controls autoPlay muted></video>
-      <h3>Channel to learn web development</h3>
+     <iframe src={`https://www.youtube.com/embed/${videoId}?autoplay=1`} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+      <h3>{apiData?apiData.snippet.title:"Title Here"}</h3>
       <div className="play-video-info">
-        <p>1525views &bull; 2days ago</p>
+        <p>{apiData?value_converter(apiData.statistics.viewCount):"16K"}Views &bull; {apiData?moment(apiData.snippet.publishedAt).fromNow():""}</p>
         <div>
-            <span><img src={like}alt=""/>125</span> 
-            <span><img src={dislike}alt=""/>2</span> 
+           <span><img src={like} alt=""/>{channelData ? value_converter(channelData.statistics?.subscriberCount) : 150}</span>
+             <span><img src={dislike}alt=""/></span> 
             <span><img src={share}alt=""/>Share</span> 
             <span><img src={save}alt=""/>Save</span> 
         </div>  
       </div>
       <hr/>
       <div className="publisher">
-        <img src={jack} alt="" />
+        <img src={channelData?channelData.snippet.thumbnails.default.url:""} alt="" />
         <div>
-             <p>GreatStack</p>
-      <span>1M Subscribers</span>
+             <p>{apiData?apiData.snippet.channelTitle:""}</p>
+      <span>{channelData?value_converter(channelData.statistics.subscriberCount):"1M"} Subscribers</span>
         </div>
         <button>Subscribe</button>
       </div>
       <div className="vid-description">
-        <p>Channel that makes learning easy</p>
-        <p>Subscribe to my channel</p>
-        <h4>120 Comments</h4>
-        <div className="comment">
-          <img src={user_profile}alt=""/>
-          <div>
-            <h3>Afra Anan <span>2 day ago</span></h3>
-            <p>A global network providing a variely of infomation of interconnected networks using standardized communication</p>
+        <p>{apiData?apiData.snippet.description.slice(0,230):"Description Here"}</p>
+       <hr/>
+        <h4>{apiData?value_converter(apiData.statistics.commentCount):100} Comments</h4>
+        {commentData.map((item,index)=>{
+          return (
+             <div className="comment">
+          <img src={item.snippet.topLevelComment.snippet.authorProfileImageUrl}alt=""/>
+          <div key={index}>
+            <h3>{item.snippet.topLevelComment.snippet.authorDisplayName} <span>2 day ago</span></h3>
+            <p>{item.snippet.topLevelComment.snippet.textDisplay}</p>
             <div className="comment-action">
               <img src={like} alt=""/>
-              <span>255</span>
+              <span>{value_converter(item.snippet.topLevelComment.snippet.likeCount)}</span>
               <img src={dislike}alt=""/>
             </div>
           </div>
         </div>
-        <div className="comment">
-          <img src={user_profile}alt=""/>
-          <div>
-            <h3>Afra Anan <span>2 day ago</span></h3>
-            <p>A global network providing a variely of infomation of interconnected networks using standardized communication</p>
-            <div className="comment-action">
-              <img src={like} alt=""/>
-              <span>255</span>
-              <img src={dislike}alt=""/>
-            </div>
-          </div>
-        </div>
-        <div className="comment">
-          <img src={user_profile}alt=""/>
-          <div>
-            <h3>Afra Anan <span>2 day ago</span></h3>
-            <p>A global network providing a variely of infomation of interconnected networks using standardized communication</p>
-            <div className="comment-action">
-              <img src={like} alt=""/>
-              <span>255</span>
-              <img src={dislike}alt=""/>
-            </div>
-          </div>
-        </div>
+          )
+        })}
+       
+       
       </div>
     </div>
   )
